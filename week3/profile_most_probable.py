ee38dc31018db2eed960dcc3cc7fa79db6c6adfb
khaeuk <khaeuk@gmail.com>
2021-02-24 23:46:06 -0800

    Parses arguments more elegantly.

--- week3/profile_most_probable.py
+++ week3/profile_most_probable.py
@@ -37,7 +37,8 @@ def parseArgs() :
 def main() :
     args = parseArgs()
     dataset_path = "{}/{}_dataset.txt".format(DATASET_DIR, os.path.splitext(sys.argv[0])[0])
-    
+    nucleotides = ["A", "C", "G", "T"]
+
     # Default to the dataset folder, if not provided
     if not args.file :
         args.file = dataset_path
@@ -45,17 +46,10 @@ def main() :
     with open(args.file, 'r') as f :
         text = f.readline().strip()
         k = f.readline().strip()
-        
+
         profile_matrix = {}
         for i, row in enumerate(f.readlines()) : 
-            if i == 0 :
-                profile_matrix["A"] = row.strip()
-            elif i == 1 :
-                profile_matrix["C"] = row.strip()
-            elif i == 2 :
-                profile_matrix["G"] = row.strip()
-            else :
-                profile_matrix["T"] = row.strip()
+            profile_matrix[nucleotides[i]] = [float(_) for _ in row.strip().split(" ")]
 
     print(profile_matrix)
 
