4c66fdf7c80b61adb7804b500ea79940a8178534
khaeuk <khaeuk@gmail.com>
2021-02-27 01:21:15 -0800

    Incomplete : Checking in current version of greedy_motif_search.py

--- week3/greedy_motif_search.py
+++ week3/greedy_motif_search.py
@@ -1,13 +1,86 @@
 import argparse
 import os
 import sys
+from profile_most_probable import profileMostProbableKmer
 from pathlib import Path
 DATASET_DIR = os.path.join(os.getcwd(), 'datasets')
 WEEK2_DIR = str(Path(__file__).resolve().parents[1]) + "/week2"
 sys.path.insert(1, WEEK2_DIR)
+from hamming_distance import hammingDistance
 
 
+def find_consensus_string(kmers):
 
+    consensus = []
+
+    for i in range(len(kmers[0])):
+        nucleotides_dict = {"A": 0, "C": 0, "G": 0, "T": 0}
+
+        for j in range(len(kmers)):
+            curr_kmer = kmers[j]
+            nucleotides_dict[curr_kmer[i]] = nucleotides_dict[curr_kmer[i]] + 1
+
+        consensus.append( max(nucleotides_dict, key=nucleotides_dict.get) )
+
+    return "".join(consensus)
+
+def update_profile_matrix(kmer, profile):
+    
+    for i in range(len(kmer)) :
+        profile[kmer[i]][i] = profile[kmer[i]][i] + 1
+
+    return profile
+
+def greedyMotifSearch(dnas, k, t) :
+
+    # Set best motif as the first kmers for each dna strings
+    best_motifs = []
+    [best_motifs.append(dna[:k]) for dna in dnas]
+    best_motif_score = float('inf')
+
+    # Initialize profile matrix
+    profileMatrix = { "A": [0] * k, "C": [0] * k, "G": [0] * k, "T": [0] * k }
+
+    count = 0
+    for i in range(len(dnas[0])-k+1):
+        motifs = []
+        consensus_motif = ""
+        curr_kmer = dnas[0][i:i+k]
+        motifs.append(curr_kmer)
+        
+        profile = update_profile_matrix(curr_kmer, profileMatrix)
+        for key, val in profile.items() :
+            profile[key] = [_/(i+1) for _ in val]
+        
+        for j in range(1, t) :
+            # Find most probable kmer given the current profile
+            motif = profileMostProbableKmer(dnas[j], k, profile)
+            motifs.append(motif)
+            
+            # Have to update profile again
+            profile = update_profile_matrix(motif, profileMatrix)
+            for key, val in profile.items() :
+                profile[key] = [_/(i+1+j) for _ in val]
+
+        consensus_motif = find_consensus_string(motifs)
+        motif_score = 0
+        for motif in motifs : 
+            motif_score = motif_score + hammingDistance(consensus_motif, motif)
+        
+        if motif_score <= best_motif_score :
+            best_motif_score = motif_score
+            best_motifs = motifs
+
+        # break
+        # for j in range(2, t):
+        # count = count + 1
+        
+        # if count == 2 :
+        #     break
+    
+
+
+    return "\n".join(best_motifs)
 
 def parseArgs() :
     parser = argparse.ArgumentParser(
@@ -50,16 +123,14 @@ def main() :
         args.file = dataset_path
     
     with open(args.file, 'r') as f :
-        text = f.readline().strip()
-        k = f.readline().strip()
+        k, t = f.readline().split(" ")
+        
+        dna_strings = []
+        [dna_strings.append(row.strip()) for i, row in enumerate(f.readlines())]
+            
+    motif = greedyMotifSearch(dna_strings, int(k), int(t))
 
-        profile_matrix = {}
-        for i, row in enumerate(f.readlines()) : 
-            profile_matrix[nucleotides[i]] = [float(_) for _ in row.strip().split(" ")]
-
-    kmer = profileMostProbableKmer(text, int(k), profile_matrix)
-
-    print(kmer)
+    print(motif)
 
 if __name__ == "__main__":
     main()
