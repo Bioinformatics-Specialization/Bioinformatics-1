16b08e631eabc8496c1478fd72da245aa8d5643d
khaeuk <khaeuk@gmail.com>
2021-03-04 23:32:07 -0800

    Cleaned up the code.

--- week4/randomized_motif_search.py
+++ week4/randomized_motif_search.py
@@ -13,23 +13,8 @@ from profile_most_probable import profileMostProbableKmer
 from distanceBetweenPatternAndStrings import distanceBetweenPatternAndStrings
 from greedy_motif_search import build_profile_matrix
 
-def build_profile_matrix(kmers, pseudocounts=False):
-    k = len(kmers[0])
-    # Initialize profile matrix
-    if pseudocounts : profileMatrix = { "A": [1] * k, "C": [1] * k, "G": [1] * k, "T": [1] * k }    
-    else : profileMatrix = { "A": [0] * k, "C": [0] * k, "G": [0] * k, "T": [0] * k }    
-    
-    for kmer in kmers :
-        for i in range(len(kmer)) :
-            profileMatrix[kmer[i]][i] = profileMatrix[kmer[i]][i] + 1
 
-    for key, val in profileMatrix.items() :
-        profileMatrix[key] = [_/len(kmers) for _ in val]
-
-    return profileMatrix
-
-
-def find_motifs_from_profile2(profile, dnas) :
+def find_motifs_from_profile(profile, dnas) :
     motifs = []
     k = len(profile["A"])
 
@@ -43,37 +28,6 @@ def find_motifs_from_profile2(profile, dnas) :
         counter = counter + 1
     
     return motifs
-
-
-
-def find_motifs_from_profile(profile, dnas) :
-    motifs = []
-    k = len(profile["A"])
-
-    counter = 1
-    for dna in dnas :
-        # print("Looking into {}th DNA : {}".format(counter, dna))
-        curr_motif = ""
-        curr_motif_score = float("-inf")
-
-        for i in range(len(dna)-k+1) :
-            curr_kmer = dna[i:i+k]
-            curr_kmer_score = 1
-
-            for j in range(len(curr_kmer)) :
-                nucleotide = curr_kmer[j]
-                curr_kmer_score = curr_kmer_score * profile[nucleotide][j]
-            # print("{} has score of {}".format(curr_kmer, curr_kmer_score))
-
-            if curr_motif_score < curr_kmer_score :
-                curr_motif = curr_kmer
-                curr_motif_score = curr_kmer_score
-
-        motifs.append(curr_motif)
-        counter = counter + 1
-    
-    return motifs
-
 
 def find_consensus_string(kmers):
 
@@ -108,59 +62,40 @@ def calculate_probability(kmer, profile_matrix) :
 def randomizedMotifSearch(dnas, k, t) :
     best_motifs = []
     best_motifs_score = None
-    # random.seed(1)
 
     # Randomly select one motif per each dna as a starting point
     for dna in dnas :
         rand_idx = random.randint(0, len(dna)-k)
         random_kmer = dna[rand_idx:rand_idx+k]
         best_motifs.append(random_kmer)
-        # print("{} -> {}".format(dna, random_kmer))
+        
     motifs = best_motifs.copy()
 
     counter = 0
     while True :
         profile = build_profile_matrix(motifs, pseudocounts=True)
-        # print("\nCalculated Profile : ")
-        # for key, val in profile.items() : print("{} : {}".format(key, val))
-
-        motifs = find_motifs_from_profile2(profile, dnas)
-        # print("Motifs selected through MostProbableKmer()...")
-        # print(motifs)
-
-        next_motifs = []
+        motifs = find_motifs_from_profile(profile, dnas)
+        
         # Score all kmers in each dna against the profile
+        next_motifs = []
         for dna in dnas :
-            # print(dna)
             dna_kmers_scores_dict = {}
+
             for i in range(len(dna)-k+1) :
                 kmer = dna[i:i+k]
                 kmer_probability_score = calculate_probability(kmer, profile)
                 dna_kmers_scores_dict[kmer] = kmer_probability_score
-                # print("{} : {}".format(kmer, kmer_probability_score))
+                
             # After scoring all kmers in one dna strand, get the max
             next_motif = max(dna_kmers_scores_dict, key=dna_kmers_scores_dict.get)
-            # print("Current dna's new motif is {}".format(next_motif))
 
 
-        # print("**********")
-        # print("**********")
-        # print("**********")
-        # print("**********")
-
-        # for motif in motifs :
-        #     print(motif)
-        
-        # print("\nBased off of profile, motifs are now ...\n{}".format(motifs))
         consensus_motif = find_consensus_string(motifs)
-        # print("------")
-        # print("consensus : {}".format(consensus_motif))
-
+        
         motifs_score = 0
         for motif in motifs : 
             motifs_score = motifs_score + hammingDistance(consensus_motif, motif)
         
-        # print("Motif score : {}".format(motifs_score))
         # Calculate score for the first best_motifs because best_score is 0 due to initialization
         if not best_motifs_score :
             consensus_best_motif = find_consensus_string(best_motifs)
@@ -169,16 +104,11 @@ def randomizedMotifSearch(dnas, k, t) :
             for best_motif in best_motifs : 
                 best_motifs_score = best_motifs_score + hammingDistance(consensus_best_motif, best_motif)
         
-        # print("\nBest Motifs is...\n{}".format(best_motifs))
-        # print("Best motif score : {}".format(best_motifs_score))
-        # print(".........")
-        # print("Motifs (score: {}) vs. Best Motifs (score: {})".format(motifs_score, best_motifs_score))
         if motifs_score < best_motifs_score :
             best_motifs = motifs[:]
             best_motifs_score = motifs_score
         else :
             return best_motifs, best_motifs_score
-
 
 
 def parseArgs() :
